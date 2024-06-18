9d9df7cc03e1d63a5deace56bd7540f64832f213
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2024-06-18 20:16:58 +0900

    微分で nussinov based な配列設計の基礎ができた

--- scripts/NN.py
+++ scripts/NN.py
@@ -1,8 +1,10 @@
 import math
 from modules.markov.kmer_mm import KmerMarkovModel
-from modules.nussinov.stochastic_nussinov import StochasticNussinov
-from modules.ss.internal_distance import DijkstraSS
-
+# from modules.nussinov.stochastic_nussinov import StochasticNussinov
+from modules.nussinov.onehot_differential_nussinov import logsumexp_nussinov, differential_traceback, simple_traceback
+# from modules.ss.internal_distance import DijkstraSS
+from modules.ss.internal_distance import DijkstraSS, floyd_warshall, dijkstra_route
+from modules.ss.util import bptuple2dotbracket
 import numpy as np
 import sys
 
@@ -14,17 +16,20 @@ import ot
 from modules.noise import SumOfGammaNoiseDistribution
 
 
-def check_convergence(f, g, epsilon, C, p, q, threshold=1e-6):
+DEBUG = False
+
+
+def check_convergence(f, g, epsilon, C, p, q, threshold=1e-7):
     n, m = len(p), len(q)
     P = torch.exp((-C + g.reshape(1, m) + f.reshape(n, 1)) / epsilon)
     return torch.allclose(P.sum(0), q, atol=threshold) and torch.allclose(P.sum(1), p, atol=threshold)
 
 
-def log_sinkhorn(p, q, C, epsilon=0.2, lambda_=1e-1, iter=100):
+def log_sinkhorn(p, q, C, epsilon, iter):
     epsilon = torch.tensor(epsilon, dtype=torch.float32, requires_grad=False)
     n, m = len(p), len(q)
-    p = torch.tensor(p, dtype=torch.float32, requires_grad=False)
-    q = torch.tensor(q, dtype=torch.float32, requires_grad=False)
+    p = torch.tensor(ot.unif(n), dtype=torch.float32, requires_grad=False)
+    q = torch.tensor(ot.unif(m), dtype=torch.float32, requires_grad=False)
     f = torch.zeros(n)
     g = torch.zeros(m)
 
@@ -43,36 +48,12 @@ def log_sinkhorn(p, q, C, epsilon=0.2, lambda_=1e-1, iter=100):
 
         if check_convergence(f, g, epsilon, C, p, q):
             congergence = True
-            print("Converged at iteration: ", t)
             break
 
     P = torch.exp((-C + g.reshape(1, m) + f.reshape(n, 1)) / epsilon)
     if not congergence:
         print("Not converged")
     return P
-
-
-def sinkhorn(p, q, C, epsilon=0.2, lambda_=1e-1, iter=100):
-    # initialize randomly
-    n, m = len(p), len(q)
-
-    p = torch.tensor(p, dtype=torch.float32, requires_grad=False)
-    q = torch.tensor(q, dtype=torch.float32, requires_grad=False)
-
-    n, m = len(p), len(q)
-    K = torch.exp(-C / epsilon)
-
-    u = torch.ones(n) / n
-
-    for t in range(iter):
-        print(K.T @ u)
-        # 要素ごとにわる
-        v = q / (K.T @ u)
-        u = p / (K @ v)
-
-    P_t = u.reshape(n, 1) * K * v.reshape(1, m)
-
-    return P_t
 
 
 def compute_gromov_wasserstein_term(a, b, Cx, Cy, P):
@@ -91,12 +72,9 @@ def compute_gromov_wasserstein_term(a, b, Cx, Cy, P):
     """
     # Calculate the weighted sum of squared distances for X
     weighted_dX_squared = torch.sum(a[:, None] * a[None, :] * (Cx ** 2))
-    print(weighted_dX_squared)
 
     # Calculate the weighted sum of squared distances for Y
     weighted_dY_squared = torch.sum(b[:, None] * b[None, :] * (Cy ** 2))
-
-    print(weighted_dY_squared)
 
     cross_term = torch.einsum('ij,ik,jl,kl->', P, Cx, Cy, P)
 
@@ -105,88 +83,106 @@ def compute_gromov_wasserstein_term(a, b, Cx, Cy, P):
     return result
 
 
-def compute_gw_transport(Cx, Cy, p, q, epsilon=0.1, lambda_=1e-3, max_iter=50, sinkhorn_iter=100):
+def compute_gw_transport(Cx, Cy, p, q, epsilon, lambda_, max_iter, sinkhorn_iter):
     # initialize randomly
     n, m = len(p), len(q)
     P_t = torch.ones(n, m) / (n * m)
 
     for t in range(max_iter):
-        # print(f"t: {t}")
-        # compute the gradient
         # C_t = -4 * np.dot(np.dot(Cx, P_t), Cy) # tensor に対応するように書き換える
         C_t = -4 * torch.mm(torch.mm(Cx, P_t), Cy) + (epsilon - lambda_) * P_t
-
-        # P_t = ot.sinkhorn(hist_blue, hist_green, cost_matrix, reg=0.001, numItermax=100, method='sinkhorn_log')
-        # P_t = sinkhorn(p, q, C_t, epsilon=epsilon,
-        #                lambda_=lambda_, iter=sinkhorn_iter)
-        P_t = log_sinkhorn(p, q, C_t, epsilon=epsilon,
-                           lambda_=lambda_, iter=sinkhorn_iter)
+        P_t = log_sinkhorn(p, q, C_t, epsilon=lambda_, iter=sinkhorn_iter)
     return P_t
 
 
-def calculateDistmat(rna_ss_matrix, alpha):  # excluding pseudoknot!
+def calculateDistmat(rna_ss_matrix, alpha, A):  # excluding pseudoknot!
     # rna secondary structure について、各塩基間の距離を計算する
     n = len(rna_ss_matrix)
-    # インデックスの差の絶対値を計算するための行列を作成
-    idx = np.arange(n)
-    dist = np.abs(idx[:, None] - idx[None, :])
-
-    # 条件に基づく値の割り当て
-    A = np.where(rna_ss_matrix == 0, dist, alpha)
-
-    return A
+    return (torch.ones(n, n) - rna_ss_matrix) * A + alpha * rna_ss_matrix
 
 
-def train_model(kmer_size, n, m, alpha, Cy, lr=1e-3, num_epochs=100, noise_scale=0.1):
+def onehot2seq(X):
+    seq = ""
+    for i in range(X.size(0)):
+        m = torch.argmax(X[i])
+        if m == 0:
+            seq += "A"
+        elif m == 1:
+            seq += "U"
+        elif m == 2:
+            seq += "G"
+        elif m == 3:
+            seq += "C"
+    print(seq)
+    return seq
+
+
+def train_model(n, m, alpha, Cy, ss_y, lr=1e-3, num_epochs=100, noise_scale=0.1, dim=4, T=1.0):
     # --- pseudo code ---
     noise = SumOfGammaNoiseDistribution(k=3, nb_iterations=40)
+    A = torch.zeros(n, n)
+    for i in range(n):
+        for j in range(n):
+            A[i, j] = torch.abs(torch.tensor(i - j))
+
     # initialization
-    num_states = 4 ** kmer_size
-    pi = np.ones(num_states)
-    transition_scores = np.ones((num_states, 4))
+    # n times 4 matrix (one hot)
+    X = torch.zeros(n, dim)
+    X.requires_grad = True
+    mask = torch.zeros(n, dim)
+    # random に 1 を入れる
+
+    # random0123seq
+    random_seq = np.random.randint(0, dim, n)
+    for i in range(n):
+        mask[i, random_seq[i]] = 1
+
+    X = X + mask
+    print(f"seq = {onehot2seq(X)}")
 
     for epoch in range(num_epochs):
+        # 全ての grad の初期化
+        X.grad = None
+
+        print(
+            f"----------------------------- Epoch: {epoch} --------------------------")
+        # noise generation
+        noise_W = noise.sample((n, n)) * noise_scale
+        noise_dp = noise.sample((n, n)) * noise_scale
+
         # ----- FORWARD -----
-
-        # Kmer Markov Model
-        noise_pi = noise.sample(pi.shape).numpy() * noise_scale
-        noise_transition_scores = noise.sample(
-            transition_scores.shape).numpy() * noise_scale
-
-        pi = pi + noise_pi
-        transition_scores = transition_scores + noise_transition_scores
-
-        kmm = KmerMarkovModel(kmer_size, pi, transition_scores)
-        _, __, X = kmm.MAP_sequence(n)
-
         # Stochastic Nussinov
-        noise_X = noise.sample(X.shape).numpy() * noise_scale
-        X += noise_X
-        sn = StochasticNussinov(X)
-        sn.nussinov()
-        ss_tuple = sn.structure
-        ss = sn.structure_matrix()
+        dp = logsumexp_nussinov(X, T)
+        if DEBUG:
+            print("dp = ", dp)
 
-        # ss matrix to weighted matrix
-        W = calculateDistmat(ss, alpha)
+        # MAP 推定 on traceback
+        ss_tuple = simple_traceback(dp + noise_dp, X, T)
+        rna_ss_matrix = torch.zeros(n, n)
+        for i, j in ss_tuple:
+            rna_ss_matrix[i, j] = rna_ss_matrix[j, i] + 1
+            rna_ss_matrix[j, i] = rna_ss_matrix[j, i] + 1
 
-        # add noeise to W
-        noise_W = noise.sample(W.shape).numpy() * noise_scale
-        W = W + noise_W
-        dijkstra = DijkstraSS(alpha=alpha, distmat=W)
-        Cx = dijkstra.dijkstra()
+        ss_dot_bracket = bptuple2dotbracket(n, ss_tuple)
+        print(f"ss : {ss_dot_bracket}")
+        print(f"y  : {ss_y}")
+
+        # convert rna_ss_matrix to ss matrix
+        W = calculateDistmat(rna_ss_matrix, alpha, A)
+
+        Cx = floyd_warshall(W + noise_W)
 
         # Cx を tensor にして、Loss (= GW) の勾配を計算できるようにする
         Cx = torch.tensor(Cx, requires_grad=True, dtype=torch.float32)
 
         # GW distance
-        p = torch.ones(n) / n
-        q = torch.ones(m) / m
-        P = compute_gw_transport(Cx, Cy, p, q)
-        print(f"P.shape = {P.shape}")
+        p = torch.tensor(ot.unif(n))
+        q = torch.tensor(ot.unif(m))
+        P = compute_gw_transport(Cx, Cy, p, q, epsilon=1e-2, lambda_=10,
+                                 max_iter=50, sinkhorn_iter=1000)
         GW = compute_gromov_wasserstein_term(p, q, Cx, Cy, P)
 
-        print(f"Epoch: {epoch}, GW distance: {GW}")
+        print(f"---------- GW distance: {GW} ----------")
 
         # ----- BACKWARD -----
         # Implement backpropagation by hand
@@ -195,39 +191,42 @@ def train_model(kmer_size, n, m, alpha, Cy, lr=1e-3, num_epochs=100, noise_scale
 
         # delta Cx
         L_Cx = Cx.grad.detach().numpy()
-        print(f"L_Cx.shape = {L_Cx.shape}")
 
         # delta W
-        dijkstra = DijkstraSS(alpha=alpha, distmat=W - lr * L_Cx + noise_W)
-        map_Cx = dijkstra.dijkstra()
-        L_W = Cx.detach().numpy() - map_Cx
-        print(f"L_W.shape = {L_W.shape}")
+        W_new = W - lr * L_Cx
+        L_W = torch.zeros(n, n)
 
-        # delta ss
-        idx = np.arange(n)
-        A = np.abs(idx[:, None] - idx[None, :])
+        print("dijkstra n^2 times")
+        for i in range(n):
+            for j in range(i+1, n):
+                R_newij = dijkstra_route(W_new + noise_W, i, j)
+                R_oldij = dijkstra_route(W + noise_W, i, j)
+                L_W += 2 * (R_newij - R_oldij)
 
-        L_ss = (np.zeros((n, n)) + alpha - A) * L_W
-        print(f"L_ss.shape = {L_ss.shape}")
+        # nussinov traceback の勾配を計算する
+        # L_z1 を考える
+        L_rna_ss_matrix = (alpha - A) * L_W
 
-        # delta X
-        X_new = X - lr * L_ss + noise_X
-        # nussinov again
-        sn = StochasticNussinov(X_new)
-        sn.nussinov()
-        ss_new = sn.structure_matrix()
-        L_X = ss - ss_new
-        L_X = L_W @ L_X
+        dp_new = dp - lr * L_rna_ss_matrix
+        new_rna_ss_matrix = torch.zeros(n, n)
+        for i, j in simple_traceback(dp_new, X, T):
+            new_rna_ss_matrix[i, j] = 1
+            new_rna_ss_matrix[j, i] = 1
+        L_dp = rna_ss_matrix - new_rna_ss_matrix
 
-        # delta pi
-        pi_new = pi - lr * L_X
-        transition_scores_new = transition_scores - lr * L_X
-        # kmm again
-        kmm = KmerMarkovModel(kmer_size, pi_new + noise_pi,
-                              transition_scores_new + noise_transition_scores)
-        _, __, X = kmm.MAP_sequence(n)
-        L_theta = X - X_new
-        L_pi, L_transition_scores = L_theta
+        # L_X
+        # dp_ij に対して X[i] の勾配を計算する
+        L_X = torch.zeros(n, dim)
+        for i in range(n):
+            for j in range(i+1, n):
+                dp_grad_ij = torch.autograd.grad(
+                    dp[i][j], X, retain_graph=True)[0]
+                L_X += dp_grad_ij * L_dp[i][j]
+
+        # print("dp_grad:", dp_grad)
+        print("grad_X = ", L_X)
+        # X.backward()
+        X = X - lr * L_X
 
 
 def test_model(model):
@@ -248,14 +247,16 @@ def main():
     y_dj.calculateDistmat(ss_matrix_y)
     m = len(y)
     Cy = y_dj.dijkstra()
-    Cy = torch.tensor(Cy, requires_grad=False, dtype=torch.float32)
+    # cy should be torch
+    Cy = torch.tensor(Cy, dtype=torch.float32)
 
     # parameters about x
-    n = 30
-    kmer_size = 3  # Length of the k-mer
+    n = 20
+    T = 3.0
+    lr = torch.tensor(1e-3, requires_grad=False, dtype=torch.float32)
 
-    pi, transition_scores = train_model(
-        kmer_size, n, m,  alpha, Cy, lr=1e-3, num_epochs=100)
+    X = train_model(n, m,  alpha, Cy, y, lr, num_epochs=100,
+                    noise_scale=0.1, dim=4, T=T)
 
 
 if __name__ == "__main__":
