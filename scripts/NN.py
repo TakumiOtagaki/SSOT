905489b02abe9dc3687b77dfbb247527d108ae45
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2024-06-19 22:25:36 +0900

    微分をより詳しく実装

--- scripts/NN.py
+++ scripts/NN.py
@@ -1,22 +1,24 @@
 import math
 from modules.markov.kmer_mm import KmerMarkovModel
+from modules.nussinov.nussinov import nussinov, traceback
 # from modules.nussinov.stochastic_nussinov import StochasticNussinov
-from modules.nussinov.onehot_differential_nussinov import logsumexp_nussinov, differential_traceback, simple_traceback
+from modules.nussinov.onehot_differential_nussinov import logsumexp_nussinov, differential_traceback, simple_traceback, onehot_basepair, traceback_MAP, onehot_basepair_grad, onehot_Relu_basepair, onehot_Relu_basepair_grad, grad_dp_X
 # from modules.ss.internal_distance import DijkstraSS
 from modules.ss.internal_distance import DijkstraSS, floyd_warshall, dijkstra_route
 from modules.ss.util import bptuple2dotbracket
 import numpy as np
 import sys
-
+import pandas as pd
 import torch
-import torch.nn as nn
-import torch.optim as optim
+
+
+from scipy.special import softmax
 
 import ot
 from modules.noise import SumOfGammaNoiseDistribution
 
 
-DEBUG = False
+DEBUG = True
 
 
 def check_convergence(f, g, epsilon, C, p, q, threshold=1e-7):
@@ -74,7 +76,8 @@ def compute_gromov_wasserstein_term(a, b, Cx, Cy, P):
     weighted_dX_squared = torch.sum(a[:, None] * a[None, :] * (Cx ** 2))
 
     # Calculate the weighted sum of squared distances for Y
-    weighted_dY_squared = torch.sum(b[:, None] * b[None, :] * (Cy ** 2))
+    # weighted_dY_squared = torch.sum(b[:, None] * b[None, :] * (Cy ** 2))
+    weighted_dY_squared = torch.zeros(1)
 
     cross_term = torch.einsum('ij,ik,jl,kl->', P, Cx, Cy, P)
 
@@ -113,11 +116,19 @@ def onehot2seq(X):
             seq += "G"
         elif m == 3:
             seq += "C"
-    print(seq)
     return seq
 
 
-def train_model(n, m, alpha, Cy, ss_y, lr=1e-3, num_epochs=100, noise_scale=0.1, dim=4, T=1.0):
+def random_vector(n, dim):  # 尖った分布。x[i] のうちある d でだけ値が大きくあって欲しい
+    X = torch.zeros(n, dim)
+    for i in range(n):
+        X[i] = torch.rand(dim)
+        X[i] = torch.exp(- (X[i] - 1) ** 2 / 0.5)
+
+    return X
+
+
+def train_model(n, m, alpha, Cy, y, ss_y, lr, num_epochs, noise_scale, dim, T, ReluThreshold, gaussian_sigma, dijkstra_scaler):
     # --- pseudo code ---
     noise = SumOfGammaNoiseDistribution(k=3, nb_iterations=40)
     A = torch.zeros(n, n)
@@ -125,50 +136,72 @@ def train_model(n, m, alpha, Cy, ss_y, lr=1e-3, num_epochs=100, noise_scale=0.1,
         for j in range(n):
             A[i, j] = torch.abs(torch.tensor(i - j))
 
+    Bias = torch.zeros(n, n) * 3  # traceback の時にノイズによって非負の値が出るのを防止する
+
     # initialization
     # n times 4 matrix (one hot)
     X = torch.zeros(n, dim)
-    X.requires_grad = True
-    mask = torch.zeros(n, dim)
-    # random に 1 を入れる
-
-    # random0123seq
-    random_seq = np.random.randint(0, dim, n)
     for i in range(n):
-        mask[i, random_seq[i]] = 1
-
-    X = X + mask
-    print(f"seq = {onehot2seq(X)}")
+        # random initialization
+        X[i] = torch.rand(dim)
+        # 1/0 に近づける
+        X[i] = torch.exp(- (X[i] - 1) ** 2 / 0.1)
+    seq0 = onehot2seq(X)
+    print(X)
 
     for epoch in range(num_epochs):
         # 全ての grad の初期化
-        X.grad = None
 
         print(
             f"----------------------------- Epoch: {epoch} --------------------------")
         # noise generation
         noise_W = noise.sample((n, n)) * noise_scale
-        noise_dp = noise.sample((n, n)) * noise_scale
+        noise_theta = noise.sample((n, n, 3)) * noise_scale
+        noise_phi = noise.sample((n, n, n)) * noise_scale
 
         # ----- FORWARD -----
+        X = torch.tensor(X, requires_grad=True, dtype=torch.float32)
+        X.grad = None
+
         # Stochastic Nussinov
-        dp = logsumexp_nussinov(X, T)
-        if DEBUG:
-            print("dp = ", dp)
+        dp = logsumexp_nussinov(X, T, gaussian_sigma, ReluThreshold)
+        dp = dp + Bias
+
+        theta = torch.zeros(n, n, 3)
+        phi = torch.zeros(n, n, n)
+        for i in range(n-1):
+            for j in range(i+1, n):
+                theta[i][j][0] = theta[i][j][0] + dp[i+1][j]
+                theta[i][j][1] = theta[i][j][1] + dp[i][j-1]
+                # gaussian basepair
+                # theta[i][j][2] = theta[i][j][2] + dp[i+1][j-1] + \
+                #     onehot_basepair(
+                #         X[i], X[j], T, gaussian_sigma) * (j - i >= 3)
+                # Relu basepair
+                theta[i][j][2] = theta[i][j][2] + dp[i+1][j-1] + \
+                    onehot_Relu_basepair(
+                        X[i], X[j], T, ReluThreshold) * (j - i >= 3)
+                for k in range(i+1, j):
+                    phi[i][j][k] = phi[i][j][k] + dp[i][k] + dp[k+1][j]
 
         # MAP 推定 on traceback
-        ss_tuple = simple_traceback(dp + noise_dp, X, T)
+        # ↓ここに X が引数となっているのはまずいのでは？
+        # ss_tuple = simple_traceback(dp + noise_dp, X, T, gaussian_sigma) # noise を足して負の項が出ないように。
+        ss_tuple, z1, z2 = traceback_MAP(
+            theta + noise_theta, phi + noise_phi, n)
         rna_ss_matrix = torch.zeros(n, n)
-        for i, j in ss_tuple:
-            rna_ss_matrix[i, j] = rna_ss_matrix[j, i] + 1
-            rna_ss_matrix[j, i] = rna_ss_matrix[j, i] + 1
+        rna_ss_matrix = rna_ss_matrix + z1[:, :, 2]
 
         ss_dot_bracket = bptuple2dotbracket(n, ss_tuple)
+        print(f"seq 1st = {seq0}")
+        print(f"seq now = {onehot2seq(X)}")
         print(f"ss : {ss_dot_bracket}")
-        print(f"y  : {ss_y}")
+        print(f"yss: {ss_y}")
+        print(f"y  : {y}")
 
         # convert rna_ss_matrix to ss matrix
         W = calculateDistmat(rna_ss_matrix, alpha, A)
+        W = W * dijkstra_scaler
 
         Cx = floyd_warshall(W + noise_W)
 
@@ -178,8 +211,8 @@ def train_model(n, m, alpha, Cy, ss_y, lr=1e-3, num_epochs=100, noise_scale=0.1,
         # GW distance
         p = torch.tensor(ot.unif(n))
         q = torch.tensor(ot.unif(m))
-        P = compute_gw_transport(Cx, Cy, p, q, epsilon=1e-2, lambda_=10,
-                                 max_iter=50, sinkhorn_iter=1000)
+        P = compute_gw_transport(Cx, Cy, p, q, epsilon=1e-1, lambda_=10 * dijkstra_scaler,
+                                 max_iter=1000, sinkhorn_iter=1000)
         GW = compute_gromov_wasserstein_term(p, q, Cx, Cy, P)
 
         print(f"---------- GW distance: {GW} ----------")
@@ -199,48 +232,107 @@ def train_model(n, m, alpha, Cy, ss_y, lr=1e-3, num_epochs=100, noise_scale=0.1,
         print("dijkstra n^2 times")
         for i in range(n):
             for j in range(i+1, n):
+                # if DEBUG:
+                #     print(f"i = {i}, j = {j}")
                 R_newij = dijkstra_route(W_new + noise_W, i, j)
                 R_oldij = dijkstra_route(W + noise_W, i, j)
-                L_W += 2 * (R_newij - R_oldij)
+                L_W += 2 * (R_oldij - R_newij)
 
         # nussinov traceback の勾配を計算する
         # L_z1 を考える
         L_rna_ss_matrix = (alpha - A) * L_W
+        L_z1 = torch.zeros(n, n, 3)
+        L_z1[:, :, 2] = L_z1[:, :, 2] + L_rna_ss_matrix
+        L_z2 = torch.zeros(n, n, n)
 
-        dp_new = dp - lr * L_rna_ss_matrix
-        new_rna_ss_matrix = torch.zeros(n, n)
-        for i, j in simple_traceback(dp_new, X, T):
-            new_rna_ss_matrix[i, j] = 1
-            new_rna_ss_matrix[j, i] = 1
-        L_dp = rna_ss_matrix - new_rna_ss_matrix
+        new_theta = theta - lr * L_z1
+        new_phi = phi - lr * L_z2
+
+        # MAP
+        new_ss_tuple, new_z1, new_z2 = traceback_MAP(new_theta + noise_theta,
+                                                     new_phi + noise_phi, n)
+        L_theta = z1 - new_z1
+        L_phi = z2 - new_z2
+
+        # もし L_theta[:,:, 2] == 0 だといかが全て 0 になるので、やり直す。ランダムな方向を X に加えてやり直す
+        if torch.allclose(L_theta[:, :, 2], torch.zeros(n, n)):
+            print("L_theta[:, :, 2] == 0; retry")
+            X = X + random_vector(n, dim) * 10
+            X = softmax(X.detach().numpy(), axis=1)
+            continue
+
+        L_dp = torch.zeros(n, n)
+        L_X = torch.zeros(n, dim)
+        for i in range(n-1):
+            for j in range(i+1, n):
+                L_dp[i][j] += L_theta[i-1][j+1][2] if i - \
+                    1 >= 0 and j+1 < n else 0
+                L_dp[i][j] += L_theta[i-1][j][0] if i-1 >= 0 else 0
+                L_dp[i][j] += L_theta[i][j+1][1] if j+1 < n else 0
+                for l in range(j+1, n):  # dp[i][j] が分岐の時に左側に出る場合
+                    L_dp[i][j] += L_phi[i][l][j] if l < n else 0
+                for l in range(i):  # dp[i][j] が分岐の時に右側に出る場合
+                    L_dp[i][j] += L_phi[l][j][i-1] if l >= 0 else 0
+        if DEBUG:
+            # print("L_z1:", pd.DataFrame(L_z1[:, :, 2].detach().numpy()))
+            print("theta:", pd.DataFrame(theta[:, :, 2].detach().numpy()))
+            print("new_theta:", pd.DataFrame(
+                new_theta[:, :, 2].detach().numpy()))
+            # print("z1 = ", pd.DataFrame(z1[:, :, 2].detach().numpy()))
+            # print("new_z1 = ", pd.DataFrame(new_z1[:, :, 2].detach().numpy()))
+            print("z1 == new_z1:", torch.allclose(z1, new_z1))
+        print("L_theta:", pd.DataFrame(L_theta[:, :, 2].detach().numpy()))
+        # sys.exit()
+
+        print("L_dp:", pd.DataFrame(L_dp.detach().numpy()))
+        for i in range(n):
+            # L_x[i, d] = sum_j(L_theta[i, j] * onehot_basepair(X[i], X[j], T, gaussian_sigma) * (j - i >= 3))
+            for j in range(i+1, n):
+                # L_X[i] += onehot_basepair_grad(
+                #     X[i], X[j], T, gaussian_sigma) * L_theta[i][j][2] * (j - i >= 3)
+                L_X[i] += onehot_Relu_basepair_grad(
+                    X[i], X[j], T, ReluThreshold) * L_theta[i][j][2]
 
         # L_X
-        # dp_ij に対して X[i] の勾配を計算する
-        L_X = torch.zeros(n, dim)
-        for i in range(n):
-            for j in range(i+1, n):
-                dp_grad_ij = torch.autograd.grad(
-                    dp[i][j], X, retain_graph=True)[0]
-                L_X += dp_grad_ij * L_dp[i][j]
+        ddpdX = grad_dp_X(dp, X, ReluThreshold, T, dim)
+        print("ddpdX:", ddpdX)
+        # ddpdX[n][dim][n][n]
+        for t in range(n):
+            for d in range(dim):
+                for i in range(n):
+                    for j in range(i+1, n):
+                        L_X[t][d] += ddpdX[t][d][i][j] * L_dp[i][j]
 
-        # print("dp_grad:", dp_grad)
-        print("grad_X = ", L_X)
-        # X.backward()
+        print("L_X:", pd.DataFrame(L_X.detach().numpy()))
+
         X = X - lr * L_X
+        X = softmax(X.detach().numpy(), axis=1)
+        print("X:", pd.DataFrame(X, dtype=float))
+        # sys.exit()
+
+        # print("L_X:", L_X)
+
+        # もし X の各行の最大のものの最小値が 1 に非常に近いならば、収束したとみなし、終了する
+        if np.min(np.max(X, axis=1)) > 0.5 and epoch > 2:
+            break
 
 
-def test_model(model):
+def test():
+    # print(torch.log(torch.tensor(0)))
+    # sys.exit()
     pass
 
 
 def main():
-    alpha = 0.5
 
+    alpha = 0.3
     # pre calculation for loss function
-    y = "((.....((...))....)...)"
-    ss_y = [(0, 22), (1, 18), (7, 12), (8, 11)]
+    y = "GGGGAAAAAACCCC"
+    dp = nussinov(y)
+    tuple_structure = traceback(dp, y, 0, len(y)-1)
+    ss_y_string = bptuple2dotbracket(len(y), tuple_structure)
     ss_matrix_y = np.zeros((len(y), len(y)), dtype=int)
-    for i, j in ss_y:
+    for i, j in tuple_structure:
         ss_matrix_y[i][j] = 1
         ss_matrix_y[j][i] = 1
     y_dj = DijkstraSS(alpha=alpha)
@@ -251,13 +343,19 @@ def main():
     Cy = torch.tensor(Cy, dtype=torch.float32)
 
     # parameters about x
-    n = 20
-    T = 3.0
-    lr = torch.tensor(1e-3, requires_grad=False, dtype=torch.float32)
-
-    X = train_model(n, m,  alpha, Cy, y, lr, num_epochs=100,
-                    noise_scale=0.1, dim=4, T=T)
+    n = m + 4
+    dim = 4
+    T = 100.0
+    noise_scale = 1e-5
+    num_epochs = 100
+    dijkstra_scaler = 10.0
+    Relu_Threshold = 0.7  # should be larger than 0.5, smaller than 1.0
+    lr = torch.tensor(4e-2, requires_grad=False, dtype=torch.float32)
+    gaussian_sigma = 0.1
+    X = train_model(n, m,  alpha, Cy, y, ss_y_string, lr, num_epochs,
+                    noise_scale, dim, T, Relu_Threshold, gaussian_sigma, dijkstra_scaler)
 
 
 if __name__ == "__main__":
+    test()
     main()
